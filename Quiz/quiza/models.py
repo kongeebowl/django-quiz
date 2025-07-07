156dcdcd4422e11930ab903895c7985fa4ce93dc
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2025-07-07 11:51:40 -0400

    a

---
+++ Quiz/quiza/models.py
@@ -0,0 +1,18 @@
+
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
