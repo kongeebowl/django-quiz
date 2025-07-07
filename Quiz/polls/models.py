aab2bfb842268825a6b318e85708e5e5a30dfdc6
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2025-07-07 12:54:54 -0400

    d

--- Quiz/polls/models.py
+++ Quiz/polls/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
--- Quiz/polls/views.py
+++ Quiz/polls/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're  the polls index.")
+
+def selected():
+    return HttpResponse("Hey this is the question u are selecting")
