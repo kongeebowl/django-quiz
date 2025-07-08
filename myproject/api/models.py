33923a48c6b59cc74d482bc71ce6f364cf995989
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2025-07-08 11:03:45 -0400

    tr

--- myproject/api/models.py
+++ myproject/api/models.py
@@ -9,7 +9,12 @@ class User(models.Model):
     def __str__(self):
         return self.username
 
-
+class Choices(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    def __str__(self):
+        return self.username
 
 
 
