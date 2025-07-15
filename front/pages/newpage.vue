337ef45ea7169e6f335869d5d918f78bddd975da
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2025-07-15 12:49:46 -0400

    g

--- front/pages/newpage.vue
+++ front/pages/newpage.vue
@@ -1,11 +1,29 @@
 <template>
     <div>
-     <AuthPage/>
+     <AuthPage :users="data"/>
      
     </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import type {User} from'@/components/AuthPage.vue'
+const data = ref<User[]|null>(null)
+async function getUsers(){
+    try {
+    const response = await fetch("http://127.0.0.1:8000/api/users/");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const fetched = await response.json();
+    data.value = fetched
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+  }
+}
+onMounted(async () => {
+ await getUsers()
+})
 </script>
 
 <style scoped>
