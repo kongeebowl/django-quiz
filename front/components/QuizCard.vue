8fa2705559ce882f9aa43b7254af6ceccb6659c1
kongeebowl <kadenpang09@gmail.com>
2025-07-17 10:36:11 -0400

    Update QuizCard.vue

--- front/components/QuizCard.vue
+++ front/components/QuizCard.vue
@@ -11,27 +11,17 @@
       >
         {{ choice }}
       </li>
-      <button @click="changeAnswered">Submit</button>
+      <button @click="">Submit</button>
     </ul>
-    <p v-if="answered" class="mt-4 text-lg text-gray-300">
-      Answer: {{ props.answer }}
-    </p>
   </div>
 </template>
 
 <script setup lang="ts">
-import { defineProps, ref } from "vue";
-
-const answered = ref(false);
-
-function changeAnswered() {
-  answered;
-}
+import { defineProps } from "vue";
 
 const props = defineProps<{
   question: string;
   choices: string[];
-  answer: string;
 }>();
 </script>
 
