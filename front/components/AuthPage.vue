cf43ff4a06478e2d80dfa79417f1c5508385d2bd
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2025-07-21 12:05:32 -0400

    s

--- front/components/AuthPage.vue
+++ front/components/AuthPage.vue
@@ -11,44 +11,67 @@
                   Sign in to
                    your account
               </h1>
-              <form class="space-y-4 md:space-y-6" action="#">
+              <form class="space-y-4 md:space-y-6" action="#" @submit.prevent="handleLogin()">
                   <div>
-                      <label for="email" class="block mb-2 text-sm font-medium text-white">Your email</label>
-                      <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="emails amiright" >
+                      <label for="email" class="block mb-2 text-sm font-medium text-white">Your username</label>
+                      <input name="username"  class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="emails amiright" v-model="username">
                   </div>
                   <div>
                       <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                      <input type="password" name="password" id="password" placeholder="paswords am i right" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                      <input  name="password" id="password" placeholder="paswords am i right" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"v-model="password">
                   </div>
                   <div class="flex items-center justify-between">
-                      <div class="flex items-start">
-                          <div class="flex items-center h-5">
-                            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" >
-                          </div>
-                          <div class="ml-3 text-sm">
-                            <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-                          </div>
-                      </div>
-                      <a href="#" class="text-sm text-white font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                      
                   </div>
                   <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Don’t have an account yet? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-                  </p>
+                  
               </form>
           </div>
       </div>
-      <div v-for="user in users"><p>{{ user.password }}</p></div>
   </div>
 </section>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-export interface User{username:string,email:string,password:string}
-const props = defineProps<{
-    users:User[]|null
-}>()
+const config =useRuntimeConfig()
+const username = ref('')
+const password = ref('')
+const error = ref(null)
+
+const handleLogin = async () => {
+  try {
+    
+    const res = await fetch('http://localhost:8000/api/signin/', {
+      method: 'POST',
+      credentials: 'include', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        username: username.value,
+        password: password.value,
+      }),
+    })
+    console.log(username.value)
+
+    console.log(res)
+    const data = await res.json()
+    console.log(data.value)
+
+    if (!res.ok) {
+        alert("no")
+      error.value = data.error
+      console.log(error.value)
+    } else {
+        alert("yes")
+      error.value = null
+      
+    }
+    console.log(error)
+  } catch (err) {
+    console.log(err)
+  }
+}
 </script>
 
 <style scoped>
--- front/pages/newpage.vue
+++ front/pages/newpage.vue
@@ -1,29 +1,13 @@
 <template>
     <div>
-     <AuthPage :users="data"/>
+     <AuthPage/>
      
     </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-import type {User} from'@/components/AuthPage.vue'
-const data = ref<User[]|null>(null)
-async function getUsers(){
-    try {
-    const response = await fetch("http://127.0.0.1:8000/api/users/");
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
-    }
-    const fetched = await response.json();
-    data.value = fetched
-  } catch (error) {
-    console.error("There has been a problem with your fetch operation:", error);
-  }
-}
-onMounted(async () => {
- await getUsers()
-})
+
+
 </script>
 
 <style scoped>
